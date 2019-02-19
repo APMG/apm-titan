b6da4f33f1bf00983643df2f6c8cf0b248fdbb83
Kim Thompson <kthompson@mpr.org>
2019-02-19 12:05:40 -0600

    heading component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Heading } from './lib';
+import { render } from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => (
+  <div>
+    <Heading title={'this is a title'} level={3}>Title</Heading>
+  </div>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<App />, document.getElementById('root'));
