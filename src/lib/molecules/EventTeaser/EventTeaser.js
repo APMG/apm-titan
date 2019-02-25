8d0b8de3e6d7ff2e2925de3ca608f520593f08e0
Kim Thompson <kthompson@mpr.org>
2019-02-25 16:38:54 -0600

    added EventTeaser and fixed tests

---
+++ src/lib/molecules/EventTeaser/EventTeaser.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from '@reach/router';
+import Heading from '../../atoms/Heading/Heading';
+import EventDates from '../Event/EventDates';
+import EventVenue from '../Event/EventVenue';
+
+const EventTeaser = (props) => {
+  return (
+    <article className="eventTeaser">
+      <div className="eventTeaser_header">
+        {props.id && (
+          <Link to={`/${props.id}`} className="event_topic link-plain">
+            {props.title && (
+              <Heading
+                level={props.headingLevel ? props.headingLevel : 1}
+                className="hdg hdg-4"
+              >
+                {props.title}
+              </Heading>
+            )}
+          </Link>
+        )}
+        {props.eventDates && <EventDates eventDates={props.eventDates} />}
+
+        {props.venue && <EventVenue venue={props.venue} />}
+      </div>
+    </article>
+  );
+};
+
+EventTeaser.propTypes = {
+  title: PropTypes.string,
+  id: PropTypes.number.isRequired,
+  eventDates: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.string.isRequired,
+      description: PropTypes.string
+    })
+  ),
+  headingLevel: PropTypes.number,
+  venue: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    street: PropTypes.string,
+    city: PropTypes.string,
+    state: PropTypes.string,
+    zip: PropTypes.string,
+    longitude: PropTypes.string,
+    latitude: PropTypes.string
+  })
+};
+
+export default EventTeaser;
