5f49747858e3e9d0a6458a5f67397c6da018ed79
Matt Aho <maho@mpr.org>
2019-04-09 10:19:28 -0500

    Teaser accepts an audio play button from props

--- src/molecules/Teaser/Teaser.js
+++ src/molecules/Teaser/Teaser.js
@@ -3,13 +3,12 @@ import PropTypes from 'prop-types';
 import Link from '../../atoms/Link/Link';
 import Heading from '../../atoms/Heading/Heading';
 import TagLink from '../../atoms/TagLink/TagLink';
-import Button from '../../atoms/Button/Button';
 import { toSentence } from '../../utils/index';
 import { format } from 'date-fns';
 
 const Teaser = (props) => {
   return (
-    <div className="teaser">
+    <article className="teaser">
       {props.tag && (
         <TagLink
           elementClass="teaser_tagLink"
@@ -18,10 +17,8 @@ const Teaser = (props) => {
         />
       )}
 
-      {props.audio && (
-        <Button size="small" type="primary">
-          {props.audioButtonSymbol}
-        </Button>
+      {props.audioPlayButton && (
+        <div className="teaser_button">{props.audioPlayButton}</div>
       )}
 
       <Link to={props.href}>
@@ -53,26 +50,14 @@ const Teaser = (props) => {
           )}
         </div>
       </Link>
-    </div>
+    </article>
   );
 };
 
 Teaser.propTypes = {
-  audio: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string,
-      credit: PropTypes.string,
-      durationHms: PropTypes.any,
-      encodings: PropTypes.shape({
-        mediaType: PropTypes.string.isRequired,
-        filename: PropTypes.string,
-        httpFilePath: PropTypes.string.isRequired
-      })
-    })
-  ),
-  audioButtonSymbol: PropTypes.any,
+  audioPlayButton: PropTypes.node,
   contributors: PropTypes.arrayOf(PropTypes.string),
-  description: PropTypes.string,
+  description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
   headingLevel: PropTypes.number.isRequired,
   href: PropTypes.string.isRequired,
   image: PropTypes.object,
@@ -82,10 +67,6 @@ Teaser.propTypes = {
     tagName: PropTypes.string.isRequired
   }),
   title: PropTypes.string.isRequired
-};
-
-Teaser.defaultProps = {
-  audioButtonSymbol: 'Play'
 };
 
 export default Teaser;
