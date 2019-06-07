1823e9ac6092d1213636881a698b6d606c367089
Kim Thompson <kthompson@mpr.org>
2019-06-07 15:10:17 -0500

    made it so that teaser in here will use the next router of the parent app

--- src/molecules/Teaser/Teaser.js
+++ src/molecules/Teaser/Teaser.js
@@ -1,13 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import PropTypes from 'prop-types';
-// import Link from '../../atoms/Link/Link';
-import Link from 'next/link';
 import Heading from '../../atoms/Heading/Heading';
 import TagLink from '../../atoms/TagLink/TagLink';
 import { toSentence } from '../../utils/index';
 import { format } from 'date-fns';
 
 const Teaser = (props) => {
+  const navigateToItem = () => {
+    props.router.push(props.href, props.as);
+  };
+
+  const handleKeyDown = (e) => {
+    let code = e.which;
+    // 13 = Return, 32 = Space
+    if (code === 13 || code === 32) {
+      navigateToItem();
+    }
+  };
+
   return (
     <article className="teaser">
       {props.tag && (
@@ -22,37 +33,40 @@ const Teaser = (props) => {
         <div className="teaser_button">{props.audioPlayButton}</div>
       )}
 
-      <Link href={props.href}>
-        <a href={props.href}>
-          <div className="teaser_image">{props.image}</div>
+      <a
+        role="link"
+        onClick={navigateToItem}
+        onKeyDown={handleKeyDown}
+        tabIndex="0"
+      >
+        <div className="teaser_image">{props.image}</div>
 
-          <div className="teaser_content">
-            <div className="teaser_header">
-              <Heading
-                level={props.headingLevel}
-                elementClass={`hdg hdg-${props.headingLevel}`}
-              >
-                {props.title}
-              </Heading>
-            </div>
-            <div className="teaser_meta">
-              {props.publishDate && (
-                <time className="teaser_time" dateTime={props.publishDate}>
-                  {format(props.publishDate, 'MMMM D, YYYY')}
-                </time>
-              )}
-              {props.contributors && (
-                <div className="teaser_contributors">
-                  By {toSentence(props.contributors)}
-                </div>
-              )}
-            </div>
-            {props.description && (
-              <div className="teaser_body userContent">{props.description}</div>
+        <div className="teaser_content">
+          <div className="teaser_header">
+            <Heading
+              level={props.headingLevel}
+              elementClass={`hdg hdg-${props.headingLevel}`}
+            >
+              {props.title}
+            </Heading>
+          </div>
+          <div className="teaser_meta">
+            {props.publishDate && (
+              <time className="teaser_time" dateTime={props.publishDate}>
+                {format(props.publishDate, 'MMMM D, YYYY')}
+              </time>
+            )}
+            {props.contributors && (
+              <div className="teaser_contributors">
+                By {toSentence(props.contributors)}
+              </div>
             )}
           </div>
-        </a>
-      </Link>
+          {props.description && (
+            <div className="teaser_body userContent">{props.description}</div>
+          )}
+        </div>
+      </a>
     </article>
   );
 };
@@ -63,6 +77,8 @@ Teaser.propTypes = {
   description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
   headingLevel: PropTypes.number.isRequired,
   href: PropTypes.string.isRequired,
+  as: PropTypes.string,
+  router: PropTypes.any,
   image: PropTypes.object,
   publishDate: PropTypes.string,
   tag: PropTypes.shape({
