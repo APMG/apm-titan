13a721411c017ae208673aeae37dccbb7b1f7e5c
Kim Thompson <kthompson@mpr.org>
2019-06-25 16:27:45 -0500

    forgot to export everything

--- src/main.js
+++ src/main.js
@@ -5,4 +5,28 @@ import Loading from './atoms/Loading/Loading';
 import Pagination from './atoms/Pagination/Pagination';
 import TagLink from './atoms/TagLink/TagLink';
 
-export { Button, Figure, Heading, Loading, Pagination, TagLink };
+// import AudioPlayer from './molecules/AudioPlayer/AudioPlayer';
+import Body from './molecules/Body/Body';
+import Content from './molecules/Content/Content';
+import Event from './molecules/Event/Event';
+import EventTeaser from './molecules/EventTeaser/EventTeaser';
+import Header from './molecules/Header/Header';
+import Hero from './molecules/Hero/Hero';
+import Teaser from './molecules/Teaser/Teaser';
+
+export {
+  Button,
+  Figure,
+  Heading,
+  Loading,
+  Pagination,
+  TagLink,
+  // AudioPlayer,
+  Body,
+  Content,
+  Event,
+  EventTeaser,
+  Header,
+  Hero,
+  Teaser
+};
