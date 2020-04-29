1cd38fdaf70d667747adeae6dc1b393a501c9541
Jason Phan <jphan@mpr.org>
2020-04-29 14:46:31 -0500

    run prettier

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,3 @@
-import '@testing-library/jest-dom'
-import 'jest-prop-type-error'
-import 'babel-polyfill'
+import '@testing-library/jest-dom';
+import 'jest-prop-type-error';
+import 'babel-polyfill';
