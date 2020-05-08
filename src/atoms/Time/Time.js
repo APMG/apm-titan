0adeca74849560ade11ce67d7cfeba8ea6f86cc8
Kim Thompson <kthompson@mpr.org>
2020-05-08 16:58:53 -0500

    no tests broken, zoned time works now, idek anymore

--- src/atoms/Time/Time.js
+++ src/atoms/Time/Time.js
@@ -1,36 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { formatDistanceToNow } from 'date-fns';
-
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-function prettyDate(dateString) {
-  let sections = dateString.split(/-|T/);
-  let year = sections[0];
-  let month = months[parseInt(sections[1] - 1)];
-  let day = parseInt(sections[2]).toString();
-
-  let timeBreak = sections[3].split(':');
-  let hour = parseInt(timeBreak[0]).toString();
-  let minute = timeBreak[1];
-  let meridiem = 'a.m.';
-
-  if (hour > 12) {
-    hour = hour - 12;
-    meridiem = 'p.m.';
-  }
-
-  return `${month} ${day}, ${year} ${hour}:${minute} ${meridiem}`;
-}
+import { utcToZonedTime, format } from 'date-fns-tz';
 
 const Time = (props) => {
+  let chicagoDate = utcToZonedTime(new Date(props.dateTime), 'America/Chicago')
+
   if (props.type === 'distance') {
     return (
       <time
         className={props.elementClass && props.elementClass}
         dateTime={props.dateTime}
         //title={format(normalTime, 'MMMM d, yyyy h:mm aa')}
-        title={prettyDate(props.dateTime)}
+        title={format(chicagoDate, 'MMMM d, yyyy h:mm aa', { timeZone: 'America/Chicago' })}
       >
         {formatDistanceToNow(new Date(props.dateTime))}
       </time>
@@ -41,7 +23,7 @@ const Time = (props) => {
         className={props.elementClass && props.elementClass}
         dateTime={props.dateTime}
       >
-        {prettyDate(props.dateTime)}
+        {format(chicagoDate, props.formatString, { timeZone: 'America/Chicago' })}
       </time>
     );
   }
