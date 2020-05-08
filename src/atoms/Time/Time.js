4eae5ae8750744d766c4990d40b1c318b80f424d
Kim Thompson <kthompson@mpr.org>
2020-05-08 15:21:07 -0500

    a bullshit fix

--- src/atoms/Time/Time.js
+++ src/atoms/Time/Time.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { format, formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow } from 'date-fns';
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+function prettyDate(dateString) {
+  let sections = dateString.split(/-|T/);
+  let year = sections[0];
+  let month = months[parseInt(sections[1] - 1)];
+  let day = parseInt(sections[2]).toString();
+
+  let timeBreak = sections[3].split(':');
+  let hour = parseInt(timeBreak[0]).toString();
+  let minute = timeBreak[1];
+  let meridiem = 'a.m.';
+
+  if (hour > 12) {
+    hour = hour - 12;
+    meridiem = 'p.m.';
+  }
+
+  return `${month} ${day}, ${year} ${hour}:${minute} ${meridiem}`;
+}
 
 const Time = (props) => {
   if (props.type === 'distance') {
@@ -8,7 +29,8 @@ const Time = (props) => {
       <time
         className={props.elementClass && props.elementClass}
         dateTime={props.dateTime}
-        title={format(new Date(props.dateTime), 'MMMM d, yyyy h:mm aa')}
+        //title={format(normalTime, 'MMMM d, yyyy h:mm aa')}
+        title={prettyDate(props.dateTime)}
       >
         {formatDistanceToNow(new Date(props.dateTime))}
       </time>
@@ -19,7 +41,7 @@ const Time = (props) => {
         className={props.elementClass && props.elementClass}
         dateTime={props.dateTime}
       >
-        {format(new Date(props.dateTime), props.formatString)}
+        {prettyDate(props.dateTime)}
       </time>
     );
   }
