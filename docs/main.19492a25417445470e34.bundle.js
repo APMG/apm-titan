1778a0b9723c249d3f244ab8dc47f2076be42a6a
Kim Thompson <kthompson@mpr.org>
2020-05-15 16:58:15 -0500

    eslint failed because storybook does not compile to code it likes. I added that folder to the ignore file

--- docs/main.19492a25417445470e34.bundle.js
+++ docs/main.19492a25417445470e34.bundle.js
@@ -1,1 +1,10 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{472:function(n,o,p){p(473),p(616),p(1268),p(1374),p(1376),p(1406),n.exports=p(1409)},535:function(n,o){}},[[472,1,2]]]);
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [0],
+  {
+    472: function(n, o, p) {
+      p(473), p(616), p(1268), p(1374), p(1376), p(1406), (n.exports = p(1409));
+    },
+    535: function(n, o) {}
+  },
+  [[472, 1, 2]]
+]);
