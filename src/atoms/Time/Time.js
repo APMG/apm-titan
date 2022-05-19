8df1fdc9c438436b245ebe896b7746a2bb0409c1
Jason Phan <jphan@mpr.org>
2022-05-19 11:45:05 -0700

    update time component to default all incoming time to default to america/chicago timezone

--- src/atoms/Time/Time.js
+++ src/atoms/Time/Time.js
@@ -7,24 +7,13 @@ import timezone from 'dayjs/plugin/timezone';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-function constructTime(timeString) {
-  let sections = timeString.split(/-|T/);
-  let year = parseInt(sections[0]);
-  let month = parseInt(sections[1] - 1);
-  let day = parseInt(sections[2]).toString();
-
-  let timeBreak = sections[3].split(':');
-  let hour = parseInt(timeBreak[0]).toString();
-  let minute = parseInt(timeBreak[1]);
-
-  return new Date(year, month, day, hour, minute);
-}
-
 const Time = (props) => {
   // If this date string is formatted like our CMS formats it, use the constructTime function above. Otherwise, just parse the date normally. Parsing our CMSs timestamps directly with new Date(string) didn't behave well with the time zone stuff.
   let cmsDateStampRegex = /\d\d\d\d-[01]\d-[0123]\dT[012]\d:[012345]\d/;
   let time = cmsDateStampRegex.test(props.dateTime)
-    ? constructTime(props.dateTime)
+    ? new Date(props.dateTime).toLocaleString('en-US', {
+        timeZone: 'America/Chicago'
+      })
     : new Date(props.dateTime);
 
   if (props.type === 'distance') {
@@ -53,7 +42,7 @@ const Time = (props) => {
 
 Time.defaultProps = {
   type: 'timestamp',
-  formatString: 'MMMM DD, YYYY',
+  formatString: 'MMMM DD, YYYY h:mm A',
   distanceFormat: 'day'
 };
 
--- src/atoms/Time/__tests__/Time.test.js
+++ src/atoms/Time/__tests__/Time.test.js
@@ -7,17 +7,33 @@ import dayjs from 'dayjs';
 afterEach(cleanup);
 
 const defaultProps = {
-  dateTime: '2019-07-18T06:00:00-05:00',
+  dateTime: '2019-07-18T02:00:00-00:00',
   formatString: 'MM-DD-YYYY'
 };
 
-test('Renders a <time> element with the default config', () => {
-  const { getByText } = render(<Time dateTime={defaultProps.dateTime} />);
-  const timeNode = getByText('July 18, 2019');
+test('Renders a <time> element with the Timezone, 10:23 PM props should expect Timezone conversion to be 5:23 PM', () => {
+  let defaultTimezoneProps = {
+    dateTime: '2022-05-10T22:23:35+00:00',
+    formatString: 'MM-DD-YYYY h:mm:ss A'
+  };
+
+  const { getByText } = render(
+    <Time dateTime={defaultTimezoneProps.dateTime} />
+  );
+  const timeNode = getByText('May 10, 2022 5:23 PM');
 
   expect(timeNode.tagName).toBe('TIME');
-  expect(timeNode.textContent).toBe('July 18, 2019');
-  expect(timeNode.getAttribute('datetime')).toBe('2019-07-18T06:00:00-05:00');
+  expect(timeNode.textContent).toBe('May 10, 2022 5:23 PM');
+  expect(timeNode.getAttribute('datetime')).toBe('2022-05-10T22:23:35+00:00');
+});
+
+test('Renders a <time> element with the default config', () => {
+  const { getByText } = render(<Time dateTime={defaultProps.dateTime} />);
+  const timeNode = getByText('July 17, 2019 9:00 PM');
+
+  expect(timeNode.tagName).toBe('TIME');
+  expect(timeNode.textContent).toBe('July 17, 2019 9:00 PM');
+  expect(timeNode.getAttribute('datetime')).toBe('2019-07-18T02:00:00-00:00');
 });
 
 test('Uses "time ago" format when type is "distance"', () => {
@@ -58,21 +74,21 @@ test('Uses "time ago" format when type is "distance"', () => {
 
 test('Formats the timestamp according to the format prop', () => {
   const { getByText } = render(<Time {...defaultProps} />);
-  const node = getByText('07-18-2019');
+  const node = getByText('07-17-2019');
 
-  expect(node.textContent).toBe('07-18-2019');
-  expect(node.getAttribute('datetime')).toBe('2019-07-18T06:00:00-05:00');
+  expect(node.textContent).toBe('07-17-2019');
+  expect(node.getAttribute('datetime')).toBe('2019-07-18T02:00:00-00:00');
 });
 
 it('Adds a class to the element when an `elementClass` prop is passed in', () => {
   const { getByText } = render(
     <Time dateTime={defaultProps.dateTime} elementClass="custom classes" />
   );
-  const node = getByText('July 18, 2019');
+  const node = getByText('July 17, 2019 9:00 PM');
 
-  expect(node.textContent).toBe('July 18, 2019');
+  expect(node.textContent).toBe('July 17, 2019 9:00 PM');
   expect(node.className).toBe('custom classes');
-  expect(node.getAttribute('datetime')).toBe('2019-07-18T06:00:00-05:00');
+  expect(node.getAttribute('datetime')).toBe('2019-07-18T02:00:00-00:00');
 });
 
 it('Throws an error when a required value is missing', () => {
