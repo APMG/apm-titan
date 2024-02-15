eddd1be4ff0dae2c06b5eb5ae85d73be9fb8cd74
Jason Phan <jphan@mpr.org>
2024-02-15 12:44:55 -0600

    add new logic for pagination on mpr-news

--- src/atoms/Pagination/__tests__/Pagination.test.js
+++ src/atoms/Pagination/__tests__/Pagination.test.js
@@ -348,4 +348,152 @@ describe('Is firstLastSeparator true', () => {
 
     expect(getAllByText('...')).toHaveLength(1);
   });
+
+  it('does not show last separator near end', () => {
+    const { large } = setup();
+    const { getAllByText } = render(
+      <Pagination
+        hasFirstAndLast={true}
+        buffer={1}
+        hrefPrefix={large.hrefPrefix}
+        asPrefix={large.asPrefix}
+        resourceType={large.resourceType}
+        currentPage={8}
+        totalPages={9}
+        inclusiveFirstLast={true}
+        firstLastSeparator="..."
+      />
+    );
+
+    expect(getAllByText('...')).toHaveLength(1);
+  });
+
+  it('does show middle symbol as "of" instead of "..." separator', () => {
+    const { large } = setup();
+    const { getAllByText } = render(
+      <Pagination
+        hasFirstAndLast={true}
+        inclusiveFirstLast={false}
+        buffer={1}
+        hrefPrefix={large.hrefPrefix}
+        asPrefix={large.asPrefix}
+        resourceType={large.resourceType}
+        currentPage={8}
+        totalPages={9}
+        middleSymbol="of"
+      />
+    );
+
+    expect(getAllByText('of')).toHaveLength(1);
+  });
+
+  it('does show middle symbol as "of" instead of "..." separator. If current page is 2 and total page is 8, it should return 2 of 8 with prev and next button present', () => {
+    const { medium } = setup();
+    const { container, getByText, getAllByText } = render(
+      <Pagination
+        hasFirstAndLast={true}
+        inclusiveFirstLast={false}
+        buffer={1}
+        hrefPrefix={medium.hrefPrefix}
+        asPrefix={medium.asPrefix}
+        resourceType={medium.resourceType}
+        currentPage={2}
+        totalPages={8}
+        middleSymbol="of"
+      />
+    );
+
+    expect(container.querySelectorAll('.pagination_link')).toHaveLength(4);
+    expect(getByText('〈').href).toBe('http://localhost/episodes/1');
+    expect(container.querySelectorAll('.pagination_link-first')[0].href).toBe(
+      'http://localhost/episodes/2'
+    );
+    expect(getAllByText('of')).toHaveLength(1);
+    expect(container.querySelectorAll('.pagination_link-last')[0].href).toBe(
+      'http://localhost/episodes/8'
+    );
+    expect(getByText('〉').href).toBe('http://localhost/episodes/3');
+  });
+
+  it('does show middle symbol as "of" instead of "..." separator. If current page is 4 and total page is 7, it should return 5 of 7 with prev and next button present', () => {
+    const { medium } = setup();
+    const { container, getByText, getAllByText } = render(
+      <Pagination
+        hasFirstAndLast={true}
+        inclusiveFirstLast={false}
+        buffer={1}
+        hrefPrefix={medium.hrefPrefix}
+        asPrefix={medium.asPrefix}
+        resourceType={medium.resourceType}
+        currentPage={4}
+        totalPages={7}
+        middleSymbol="of"
+      />
+    );
+
+    expect(container.querySelectorAll('.pagination_link')).toHaveLength(4);
+    expect(getByText('〈').href).toBe('http://localhost/episodes/3');
+    expect(container.querySelectorAll('.pagination_link-first')[0].href).toBe(
+      'http://localhost/episodes/4'
+    );
+    expect(getAllByText('of')).toHaveLength(1);
+    expect(container.querySelectorAll('.pagination_link-last')[0].href).toBe(
+      'http://localhost/episodes/7'
+    );
+    expect(getByText('〉').href).toBe('http://localhost/episodes/5');
+  });
+
+  it('does show middle symbol as "of" instead of "..." separator. If current page is 1 and total page is 2, it should return 1 of 2 with prev button not present', () => {
+    const { medium } = setup();
+    const { container, getByText, getAllByText } = render(
+      <Pagination
+        hasFirstAndLast={true}
+        inclusiveFirstLast={false}
+        buffer={1}
+        hrefPrefix={medium.hrefPrefix}
+        asPrefix={medium.asPrefix}
+        resourceType={medium.resourceType}
+        currentPage={1}
+        totalPages={2}
+        middleSymbol="of"
+      />
+    );
+
+    expect(container.querySelectorAll('.pagination_link')).toHaveLength(3);
+    expect(container.querySelectorAll('.pagination_link-first')[0].href).toBe(
+      'http://localhost/episodes/1'
+    );
+    expect(getAllByText('of')).toHaveLength(1);
+    expect(container.querySelectorAll('.pagination_link-last')[0].href).toBe(
+      'http://localhost/episodes/2'
+    );
+    expect(getByText('〉').href).toBe('http://localhost/episodes/2');
+  });
+
+  it('does show middle symbol as "of" instead of "..." separator. If current page is 8 and total page is 8, it should return 8 of 8 with next button not present', () => {
+    const { medium } = setup();
+    const { container, getByText, getAllByText } = render(
+      <Pagination
+        hasFirstAndLast={true}
+        inclusiveFirstLast={false}
+        buffer={1}
+        hrefPrefix={medium.hrefPrefix}
+        asPrefix={medium.asPrefix}
+        resourceType={medium.resourceType}
+        currentPage={8}
+        totalPages={8}
+        middleSymbol="of"
+      />
+    );
+
+    expect(container.querySelectorAll('.pagination_link')).toHaveLength(3);
+    expect(getByText('〈').href).toBe('http://localhost/episodes/7');
+    expect(container.querySelectorAll('.pagination_link-first')[0].href).toBe(
+      'http://localhost/episodes/8'
+    );
+    expect(getAllByText('of')).toHaveLength(1);
+    expect(container.querySelectorAll('.pagination_link-last')[0].href).toBe(
+      'http://localhost/episodes/8'
+    );
+  });
 });
